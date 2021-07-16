5507947d71cce0a2d770231a6d314db80147f8b0
veritasnz <7684225+veritasnz@users.noreply.github.com>
2021-07-16 14:00:31 +0900

    Removed .o-drop-in

--- components/Layout/Header/Header.js
+++ components/Layout/Header/Header.js
@@ -8,7 +8,7 @@ import LangButton from "./LangButton";
 
 function Header() {
     return (
-        <header className="header o-drop-in">
+        <header className="header">
             <SocialDropdown />
             <div className="header__options">
                 <LangButton />
