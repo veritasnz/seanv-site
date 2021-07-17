4d656eba8bc1b2ce327909afda546d0f6dd4d7e1
veritasnz <7684225+veritasnz@users.noreply.github.com>
2021-07-17 18:31:34 +0900

    Moved Prose.module.scss to global stylesheet

--- components/Posts/PostBody.js
+++ components/Posts/PostBody.js
@@ -4,15 +4,13 @@ import { getMDXComponent } from "mdx-bundler/client";
 import { H2, H3 } from "../MDX/Headings";
 import { ATag } from "../MDX/Links.jsx";
 
-import s from "./Prose.module.scss";
-
 export default function PostBody(props) {
     const { content, lang = "en" } = props;
 
     const BodyComponent = useMemo(() => getMDXComponent(content), [content]);
 
     return (
-        <div className={s["prose"]} data-lang={lang}>
+        <div className="o-prose" data-lang={lang}>
             <BodyComponent
                 components={{
                     h2: H2,
