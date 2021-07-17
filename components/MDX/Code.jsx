4587a12a3baef43c323725478e592237c181a9c3
veritasnz <7684225+veritasnz@users.noreply.github.com>
2021-07-17 13:07:24 +0900

    Reverted changes to <Highlight> contents

--- components/MDX/Code.jsx
+++ components/MDX/Code.jsx
@@ -21,9 +21,9 @@ export default function Code(props) {
             {({ className, style, tokens, getLineProps, getTokenProps }) => (
                 <pre className={className} style={style}>
                     {tokens.map((line, i) => (
-                        <div {...getLineProps({ line, key: i })}>
+                        <div key={i} {...getLineProps({ line, key: i })}>
                             {line.map((token, key) => (
-                                <span {...getTokenProps({ token, key })} />
+                                <span key={key} {...getTokenProps({ token, key })} />
                             ))}
                         </div>
                     ))}
