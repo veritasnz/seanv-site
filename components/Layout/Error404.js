0b36d6461c33eac190158839a3acc47faab63475
veritasnz <7684225+veritasnz@users.noreply.github.com>
2021-07-18 10:29:48 +0900

    Moved 404 contents into own component. Used by pages/404.js & [slug] now

---
+++ components/Layout/Error404.js
@@ -0,0 +1,41 @@
+import { NextSeo } from "next-seo";
+import useTranslation from "next-translate/useTranslation";
+import Trans from "next-translate/Trans";
+
+import PageTitle from "./Second/PageTitle";
+import Container from "../UI/Container";
+import LinkButton from "../UI/LinkButton";
+
+export default function Error404() {
+    const { t } = useTranslation("common");
+
+    const breadcrumbs = [{ name: "404", url: null }];
+
+    return (
+        <>
+            <NextSeo title={"404"} />
+            <PageTitle title={t("404-title")} breadcrumbs={breadcrumbs} />
+            <Container type="second" width="thin">
+                <div className="t-404">
+                    <h2 className="t-404__subtitle o-subtitle">
+                        <Trans
+                            i18nKey="common:404-subtitle"
+                            components={[<br key="0" />]}
+                        />
+                    </h2>
+                    <p className="t-404__text">
+                        <Trans
+                            i18nKey="common:404-text"
+                            components={[<br key="0" />]}
+                        />
+                    </p>
+                    <div className="t-404__bttn">
+                        <LinkButton href="/" color="orange">
+                            {t("404-button")}
+                        </LinkButton>
+                    </div>
+                </div>
+            </Container>
+        </>
+    );
+}
--- pages/404.js
+++ pages/404.js
@@ -1,45 +1,10 @@
-import { NextSeo } from "next-seo";
-import useTranslation from "next-translate/useTranslation";
-import Trans from "next-translate/Trans";
-
-import PageTitle from "../components/Layout/Second/PageTitle";
-import Container from "../components/UI/Container";
+import Error404 from "../components/Layout/Error404";
 import PageTransitionWrapper from "../components/Layout/PageTransitionWrapper";
-import LinkButton from "../components/UI/LinkButton";
 
-export default function Custom404() {
-    const { t } = useTranslation("common");
-
-    const breadcrumbs = [{ name: "404", url: null }];
-
+export default function Page404() {
     return (
-        <>
-            <NextSeo title={"404"} />
-
-            <PageTransitionWrapper>
-                <PageTitle title={t("404-title")} breadcrumbs={breadcrumbs} />
-                <Container type="second" width="thin">
-                    <div className="t-404">
-                        <h2 className="t-404__subtitle o-subtitle">
-                            <Trans
-                                i18nKey="common:404-subtitle"
-                                components={[<br key="0" />]}
-                            />
-                        </h2>
-                        <p className="t-404__text">
-                            <Trans
-                                i18nKey="common:404-text"
-                                components={[<br key="0" />]}
-                            />
-                        </p>
-                        <div className="t-404__bttn">
-                            <LinkButton href="/" color="orange">
-                                {t("404-button")}
-                            </LinkButton>
-                        </div>
-                    </div>
-                </Container>
-            </PageTransitionWrapper>
-        </>
+        <PageTransitionWrapper>
+            <Error404 />
+        </PageTransitionWrapper>
     );
 }
