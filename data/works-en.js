a9fb5eebe2d12edb058e6fd6396afc167eb7c4d3
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-04-29 14:04:35 +1200

    style: added demo note to MapLink

--- data/works-en.js
+++ data/works-en.js
@@ -108,7 +108,7 @@ const works = [
         type: "Client Work - Development",
         stack: ["react", "svg", "javascript"],
         summary:
-            "A React-based map-manipulation tool. Pan, paint, and zoom the map, and then download the final product as a PNG file",
+            "A React-based map-manipulation tool. Pan, paint, and zoom the map, and then download the final product as a PNG file. Note the demo is in Japanese.",
         links: [
             {
                 icon: "article",
