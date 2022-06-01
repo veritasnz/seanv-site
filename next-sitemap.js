b41b31030121f48f8ffe10f4ccd3623dd12933a6
veritasnz <7684225+veritasnz@users.noreply.github.com>
2022-06-01 20:41:41 +1200

    fix: update sitemap.xml to include all EN links

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,20 @@
 module.exports = {
     siteUrl: "https://seanv.dev",
     generateRobotsTxt: true,
-    exclude: ["/en/*", "/en"],
+    transform: async (config, path) => {
+        // custom function to remove the /en/ from English links
+
+        let newPath = path;
+        if (path[1] === "e" && path[2] === "n") {
+            newPath = path.slice(3, path.length);
+        }
+
+        return {
+            loc: newPath, // => this will be exported as http(s)://<config.siteUrl>/<path>
+            changefreq: config.changefreq,
+            priority: config.priority,
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+            alternateRefs: config.alternateRefs ?? [],
+        };
+    },
 };
