99370baf65f1d8dbbbb6c3005cd470026d14e2ee
veritasnz <7684225+veritasnz@users.noreply.github.com>
2024-01-05 16:09:56 +1300

    fix: attempt redo file importing for MDX components

--- components/MDX/ExampleMDXComponent.jsx
+++ components/MDX/ExampleMDXComponent.jsx
@@ -1,21 +1,21 @@
 export * from "react";
 
-import Clouds from "../UI/Animations/Clouds";
-import Hill from "../UI/Animations/Hill";
+import Clouds from "./Clouds";
+import Hill from "./Hill";
 
 function ExampleMDXComponent() {
-    return (
-        <div className="t-example-mdx">
-            <div className="t-example-mdx__clouds">
-                <Clouds count="3" yOffset="4rem" />
-                <Clouds count="3" yOffset="10rem" />
-            </div>
-            <div className="t-example-mdx__hills">
-                <Hill height={60} color="blue" />
-                <Hill height={30} color="orange" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="t-example-mdx">
+      <div className="t-example-mdx__clouds">
+        <Clouds count="3" yOffset="4rem" />
+        <Clouds count="3" yOffset="10rem" />
+      </div>
+      <div className="t-example-mdx__hills">
+        <Hill height={60} color="blue" />
+        <Hill height={30} color="orange" />
+      </div>
+    </div>
+  );
 }
 
 export default ExampleMDXComponent;
